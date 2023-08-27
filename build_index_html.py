7fd2c873186937a15860f3c2f82ddce3dc625848
jcosto <jcosto@somnomed.com>
2023-08-27 23:41:44 +0800

    update build py

--- build_index_html.py
+++ build_index_html.py
@@ -1,46 +1,47 @@
 index_path = r"templates\index-azurewebpubsub.html"
-css_path = r"static\assets\card.css"
-main_js_path = r"static\main_pubsub.js"
-deck_js_path = r"static\components\deck_pubsub.js"
-card_js_path = r"static\components\card_pubsub.js"
-
 with open(index_path,"r") as fin:
     index_html = fin.read()
-with open(css_path, "r") as fin:
-    index_html = index_html.replace(
-        """<link rel="stylesheet" href="{{url_for('static', filename='assets/card.css')}}" />""",
-        "<style>"+fin.read()+"</style>"
-    )
-with open(main_js_path, "r") as fin:
-    index_html = index_html.replace(
-        """<script src="{{url_for('static', filename='main_pubsub.js')}}"></script>""",
-        "<script>"+fin.read()+"</script>"
-    )
-with open(deck_js_path, "r") as fin:
-    index_html = index_html.replace(
-        """<script src="{{url_for('static', filename='components/deck_pubsub.js')}}"></script>""",
-        "<script>"+fin.read()+"</script>"
-    )
-with open(card_js_path, "r") as fin:
-    index_html = index_html.replace(
-        """<script src="{{url_for('static', filename='components/card_pubsub.js')}}"></script>""",
-        "<script>"+fin.read()+"</script>"
-    )
-with open(card_js_path, "r") as fin:
-    index_html = index_html.replace(
-        """<script src="{{url_for('static', filename='components/card_deck_pubsub.js')}}"></script>""",
-        "<script>"+fin.read()+"</script>"
-    )
-with open(card_js_path, "r") as fin:
-    index_html = index_html.replace(
-        """<script src="{{url_for('static', filename='components/card_table_pubsub.js')}}"></script>""",
-        "<script>"+fin.read()+"</script>"
-    )
-with open(card_js_path, "r") as fin:
-    index_html = index_html.replace(
-        """<script src="{{url_for('static', filename='components/card_session_pubsub.js')}}"></script>""",
-        "<script>"+fin.read()+"</script>"
-    )
+
+file_replace_items = [
+    {
+        "src_path": r"static\assets\card.css",
+        "src_target": """<link rel="stylesheet" href="{{url_for('static', filename='assets/card.css')}}" />""",
+        "dst_text": "<style>{file_content}</style>"
+    },
+    {
+        "src_path": r"static\main_pubsub.js",
+        "src_target": """<script src="{{url_for('static', filename='main_pubsub.js')}}"></script>""",
+        "dst_text": "<script>{file_content}</script>"
+    },
+    {
+        "src_path": r"static\card_pubsub.js",
+        "src_target": """<script src="{{url_for('static', filename='card_pubsub.js')}}"></script>""",
+        "dst_text": "<script>{file_content}</script>"
+    },
+    {
+        "src_path": r"static\card_deck_pubsub.js",
+        "src_target": """<script src="{{url_for('static', filename='card_deck_pubsub.js')}}"></script>""",
+        "dst_text": "<script>{file_content}</script>"
+    },
+    {
+        "src_path": r"static\card_table_pubsub.js",
+        "src_target": """<script src="{{url_for('static', filename='card_table_pubsub.js')}}"></script>""",
+        "dst_text": "<script>{file_content}</script>"
+    },
+    {
+        "src_path": r"static\card_session_pubsub.js",
+        "src_target": """<script src="{{url_for('static', filename='card_session_pubsub.js')}}"></script>""",
+        "dst_text": "<script>{file_content}</script>"
+    },
+]
+for fri in file_replace_items:
+    src_path = fri["src_path"]
+    src_target = fri["src_target"]
+    dst_text = fri["dst_text"]
+    with open(src_path, 'r') as fin:
+        index_html = index_html.replace(
+            src_target, dst_text.replace("{file_content}", fin.read())
+        )
 
 with open("index.html", "w") as fout:
     fout.write(index_html)
